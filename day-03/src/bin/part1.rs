6de90494590fe5d70720e2c90e90adecd281d580
Mikuláš Zelinka <mikulas@zelinka.dev>
2023-12-02 11:03:49 +0100

    day: 03 stub

---
+++ day-03/src/bin/part1.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let input = include_str!("./input.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(input: &str) -> String {
+    input.trim().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        assert_eq!(part1(include_str!("example1.txt")), "example1");
+    }
+}
