ac02de65674359425d296d4ff48ff685f3c216c9
Mikuláš Zelinka <mikulas@zelinka.dev>
2023-12-07 22:09:19 +0100

    day: 06 (python, naive)

---
+++ day-06/main.py
@@ -0,0 +1,47 @@
+"""
+Python because this was done while teaching a Python class.
+
+This most naive solution unfortunately still finishes in under two minutes for part 2.
+"""
+
+import numpy as np
+from tqdm import tqdm
+
+
+def compute_distance(T, hold_time):
+    return (T - hold_time) * hold_time
+
+
+def solve(input_file_path, part="part1"):
+    print(input_file_path, part)
+
+    with open(input_file_path) as f:
+        if part == "part1":
+            times = [int(x) for x in f.readline().split()[1:]]
+            records = [int(x) for x in f.readline().split()[1:]]
+        elif part == "part2":
+            times = [int("".join(f.readline().split()[1:]).replace(" ", ""))]
+            records = [int("".join(f.readline().split()[1:]).replace(" ", ""))]
+
+    num_ways_to_beat_record = []
+
+    for T, record in tqdm(zip(times, records)):
+        num_better_than_record = 0
+        for hold_time in tqdm(range(T + 1)):
+            d = compute_distance(T, hold_time)
+            if d > record:
+                num_better_than_record += 1
+
+        num_ways_to_beat_record.append(num_better_than_record)
+
+    print(times)
+    print(records)
+    print(np.prod(num_ways_to_beat_record))
+    print()
+
+
+solve("example.txt")
+solve("input.txt")
+
+solve("example.txt", part="part2")
+solve("input.txt", part="part2")
