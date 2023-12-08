7ff993171b3a4c17a05bfc1f365d8a6056b1298d
Mikuláš Zelinka <mikulas@zelinka.dev>
2023-12-08 11:05:58 +0100

    day: 08 part 2 (naive, doesn't finish, ~300M steps per minute. ~20B steps per hour)

--- day-08/src/bin/part2.rs
+++ day-08/src/bin/part2.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+use indicatif::{ProgressBar, ProgressStyle};
+
 fn main() {
     let input = include_str!("./input.txt");
     let output = part2(input);
@@ -12,28 +14,48 @@ fn part2(input: &str) -> usize {
 
     assert!(lines.next().expect("second (empty) line").is_empty());
 
-    let mut network = HashMap::new();
+    let mut network: HashMap<&str, (&str, &str)> = HashMap::new();
+    let mut current_nodes: Vec<&str> = vec![];
     lines.for_each(|line| {
-        network.insert(&line[0..3], (&line[7..10], &line[12..15]));
+        let key = &line[0..3];
+        network.insert(key, (&line[7..10], &line[12..15]));
+
+        if key.ends_with('A') {
+            current_nodes.push(key);
+        }
     });
 
     dbg!(&instructions);
     dbg!(&network);
 
-    let mut current_node = "AAA";
+    let bar = ProgressBar::new(0);
+
+    let style = ProgressStyle::default_bar()
+        .template("[{elapsed_precise}] [{human_pos}] - {per_sec}")
+        .expect("valid style");
+    bar.set_style(style);
 
     for (i, instruction) in instructions.chars().cycle().enumerate() {
-        let (left, right) = network.get(current_node).expect("current node exists");
-        let next_node = match instruction {
-            'L' => left,
-            'R' => right,
-            _ => panic!("unknown instruction: {}", instruction),
-        };
-        if next_node == &"ZZZ" {
+        let mut next_nodes: Vec<&str> = vec![];
+
+        for current_node in current_nodes {
+            let (left, right) = network.get(current_node).expect("current node exists");
+            let next_node = match instruction {
+                'L' => left,
+                'R' => right,
+                _ => panic!("unknown instruction: {}", instruction),
+            };
+
+            // dbg!(i + 1, instruction, current_node, next_node);
+            next_nodes.push(next_node);
+        }
+
+        if next_nodes.iter().all(|node| node.ends_with('Z')) {
             return i + 1;
         }
-        dbg!(i + 1, instruction, current_node, next_node);
-        current_node = next_node;
+
+        current_nodes = next_nodes;
+        bar.inc(1);
     }
 
     panic!("no solution found");
@@ -47,5 +69,6 @@ mod tests {
     fn example() {
         assert_eq!(part2(include_str!("example1.txt")), 2);
         assert_eq!(part2(include_str!("example2.txt")), 6);
+        assert_eq!(part2(include_str!("example3.txt")), 6);
     }
 }
